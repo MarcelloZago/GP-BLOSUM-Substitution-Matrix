f06aef10c96b921d741620a3271c65885f1912d6
MarcelloZago <marcello.zago.99@gmail.com>
2022-01-07 13:19:16 +0100

    Add identity filtering

--- Sub_matrix.py
+++ Sub_matrix.py
@@ -29,7 +29,7 @@ class SubstitutionMatrix:
     @staticmethod
     def count_frequencies_in_file(frequency_table: pd.DataFrame, path: str):
         """
-        Function to count frequencies of change in aminoacids.
+        Function to count frequencies of change in amino acids.
         Compares all pairs of possible sequences in a file.
         Sums into a pd table of amino acid change.
 
@@ -69,8 +69,9 @@ class SubstitutionMatrix:
                             frequency_table[letter_first_seq][letter_second_seq] += 1
 
         return frequency_table
+
     @staticmethod
-    def read_directory(path: str):
+    def read_directory(path: str, identity_value: int):
         """
         Function to read in multiple preprocessed fasta files in a directory and calculate the frequencies.
         Internally just reads in every file individually and calculates the frequencies.
@@ -80,6 +81,9 @@ class SubstitutionMatrix:
         ----------
         path : str
             Path to a directory that contains the fasta files we want to read in. It can include other files.
+        identity_value : int
+            The identity threshold for the use of the alignments for the calculation.
+
 
         Returns
         -------
@@ -88,12 +92,11 @@ class SubstitutionMatrix:
 
         assert os.path.isdir(path), 'Path is not a directory'  # assure that path is a directory
 
-        # list of aminoacids
+        # list of amino acids
         aa_list = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
 
         # generate frequencies table to sum
         frequency_table = pd.DataFrame(np.zeros((len(aa_list), len(aa_list))), columns=aa_dict, index=aa_dict)
-
 
         # iterate over all files in the directory and calculate the added frequencies
         for filename in os.listdir(path):
@@ -102,38 +105,60 @@ class SubstitutionMatrix:
             if not (filename.endswith('.fa') or filename.endswith('.fasta')):
                 continue
 
-            frequency_table = SubstitutionMatrix.count_frequencies_in_file(frequency_table, f'{path}/{filename}')
-
+            # check if the identity can be used, if not skip the file
+            if SubstitutionMatrix.__is_usable_identity(identity_value, filename):
+                frequency_table = SubstitutionMatrix.count_frequencies_in_file(frequency_table, f'{path}/{filename}')
 
         return frequency_table
 
     @staticmethod
-    def sub_matrix(path: str):
-        aa_dict = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K",
+    def __is_usable_identity(wanted_identity: int, filename: str) -> bool:
+        """
+        Private function that checks whether the given file can be used with the given identity threshold.
+
+        Parameters
+        ----------
+        wanted_identity : int
+            The threshold value for the matrix we want to calculate.
+        filename : str
+            The file name of the file that is currently checked.
+        Returns
+        -------
+        True if the identity of the given alignment is smaller than the given identity threshold.
+        """
+
+        split_filename = filename.split('_')
+        parsed_identity = int(split_filename[-3])  # the actual identity of the file
+
+        return wanted_identity >= parsed_identity
+
+    @staticmethod
+    def sub_matrix(path: str, identity_value: int):
+        aa_list = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K",
                    "M", "F", "P", "S",
                    "T", "W", "Y", "V"]
-        table = SubstitutionMatrix.read_directory(path)
-        # Calculate the probability of occurence for each amino acid pair
+        table = SubstitutionMatrix.read_directory(path, identity_value=identity_value)
+        # Calculate the probability of occurrence for each amino acid pair
         all_pairs = np.triu(table).sum()
-        table = table / all_pairs
+        table = table / all_pairs  # q
 
-        # Calculate the probability of occurence for each amino acid
+        # Calculate the probability of occurrence for each amino acid
         table_pi = {"A": 0, "R": 0, "N": 0, "D": 0, "C": 0, "Q": 0, "E": 0, "G": 0, "H": 0, "I": 0, "L": 0, "K": 0,
                     "M": 0, "F": 0, "P": 0, "S": 0,
                     "T": 0, "W": 0, "Y": 0, "V": 0}
 
         table["sum"] = table.sum(axis=1)
 
-        for i in aa_dict:
+        for i in aa_list:
             table["sum"][i] = table["sum"][i] - table[i][i]
 
-        for i in aa_dict:
-            table_pi[i] = table[i][i] + (1 / 2) * table["sum"][i]
+        for i in aa_list:
+            table_pi[i] = table[i][i] + (1 / 2) * table["sum"][i]  # p
 
         # Calculate the expected probability
-        exp_table = pd.DataFrame(np.zeros((len(aa_dict), len(aa_dict))), columns=aa_dict, index=aa_dict)
-        for i in aa_dict:
-            for j in aa_dict:
+        exp_table = pd.DataFrame(np.zeros((len(aa_list), len(aa_list))), columns=aa_list, index=aa_list)
+        for i in aa_list:
+            for j in aa_list:
                 if i == j:
                     exp_table[i][j] = table_pi[i] * table_pi[j]
                 else:
@@ -141,14 +166,13 @@ class SubstitutionMatrix:
 
         # Calculate the log ratio
 
-        sub_matrix = pd.DataFrame(np.zeros((len(aa_dict), len(aa_dict))), columns=aa_dict, index=aa_dict)
+        sub_matrix = pd.DataFrame(np.zeros((len(aa_list), len(aa_list))), columns=aa_list, index=aa_list)
 
-        for i in aa_dict:
-            for j in aa_dict:
+        for i in aa_list:
+            for j in aa_list:
                 sub_matrix[i][j] = round(2 * math.log((table[i][j] / exp_table[i][j]), 2))
 
         return sub_matrix
-
 
 
 if __name__ == '__main__':
@@ -157,7 +181,7 @@ if __name__ == '__main__':
     Test 1
     """
 
-    #print(SubstitutionMatrix.count_aa_in_seq("AGGGAAAYYY"))
+    # print(SubstitutionMatrix.count_aa_in_seq("AGGGAAAYYY"))
 
     """
     Test 2: Testing SubstitutionMatrix.count_frequencies_in_file with "_7_processed_0.fasta"
@@ -173,7 +197,7 @@ if __name__ == '__main__':
     #
     # out_matrix = SubstitutionMatrix.count_frequencies_in_file(frequency_table, path)
 
-    #print(out_matrix)
+    # print(out_matrix)
 
     """
     Test 3: Testing SubstitutionMatrix.read_directory with "processed" directory
@@ -181,13 +205,16 @@ if __name__ == '__main__':
 
     path = "processed\\"
 
-    #table= SubstitutionMatrix.read_directory(path)
+    # table= SubstitutionMatrix.read_directory(path)
 
-    #print(table)
-
+    # print(table)
 
     """
     Calculate the substitution matrix
     """
-    sub_matrix = SubstitutionMatrix.sub_matrix(path)
+    identity_threshold = 62
+
+    sub_matrix = SubstitutionMatrix.sub_matrix(path, identity_value=identity_threshold)
     print(sub_matrix.to_string)
+
+    sub_matrix.to_csv(f'matrices\\our_matrix_{identity_threshold}.csv')
